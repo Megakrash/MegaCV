de6593d6b573910292626940eafbb19e70ea5260
Jonathan Scattolini <jscattolini@gmail.com>
2023-02-26 11:52:44 +0100

    :hammer: & :lipstick: :card_file_box:

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,68 @@
 import React from "react";
+import Typist from "react-typist";
 
 export default function Home() {
+  const line1 = "{";
+  const line2 = "properties: [";
+  const line3 = " ],";
+  const line4 = "};";
+
   return (
     <div className="home">
-      <p>YOUHOU</p>
+      <Typist
+        className="home_textBox"
+        avgTypingDelay={70}
+        cursor={{ show: false, hideWhenDone: true, hideWhenDoneDelay: 0 }}
+      >
+        <p className="home_textBox_line1">
+          const <span>helloTous</span> = {line1}{" "}
+        </p>
+        <Typist.Delay ms={500} />
+        <p className="home_textBox_line2">
+          firstname: <span>'Jonathan'</span>,
+        </p>
+        <Typist.Delay ms={500} />
+        <p className="home_textBox_line3">
+          lastname: <span>'Scattolini'</span>,
+        </p>
+        <Typist.Delay ms={500} />
+        <p className="home_textBox_line4">
+          location: <span>'Nantes'</span>,
+        </p>
+        <Typist.Delay ms={500} />
+        <p className="home_textBox_line5">
+          available: <span>'true'</span>,
+        </p>
+        <Typist.Delay ms={500} />
+        <p className="home_textBox_line6">{line2}</p>
+        <Typist.Delay ms={500} />
+        <p className="home_textBox_line7">
+          <span>'ReactJs'</span>,
+        </p>
+        <Typist.Delay ms={250} />
+        <p className="home_textBox_line8">
+          <span>'Sass'</span>,
+        </p>
+        <Typist.Delay ms={250} />
+        <p className="home_textBox_line9">
+          <span>'Express'</span>,
+        </p>
+        <Typist.Delay ms={250} />
+        <p className="home_textBox_line10">
+          <span>'Node Js'</span>,
+        </p>
+        <Typist.Delay ms={250} />
+        <p className="home_textBox_line11">
+          <span>'JavaScript'</span>,
+        </p>
+        <Typist.Delay ms={250} />
+        <p className="home_textBox_line12">
+          <span>'and more...'</span>
+          {line3}
+        </p>
+        <Typist.Delay ms={500} />
+        <p className="home_textBox_line13">{line4}</p>
+      </Typist>
     </div>
   );
 }
